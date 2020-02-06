840d14085bc1da91b9e88fba5942c930e94aa3ce
Deividas <deivis124@gmail.com>
2020-02-06 03:39:34 +0200

    Creates Object and Its Prototype

--- Greetr.js
+++ Greetr.js
@@ -1,3 +1,30 @@
-// Creats new Execution Context for framework
+//Creats new Execution Context for framework
+(function(global, $) {
+  console.log("hi 0");
+  var Greetr = function(firstName, lastName, language) {
+    console.log("hi 6");
+    return new Greetr.init(firstName, lastName, language);
+  };
+  console.log("hi 1");
+  Greetr.prototype = {};
+  console.log("hi 2");
 
-(function(global, $) {})(window, jQuery);
+  Greetr.init = function(firstName, lastName, language) {
+    console.log("hi 7");
+    var self = this;
+    self.firstName = firstName || "Default Jonas";
+    self.lastName = lastName || "Default Petraitis";
+    self.language = language || "Default en";
+  };
+
+  console.log("hi 3");
+  Greetr.init.protoype = Greetr.prototype;
+  console.log("hi 4");
+  global.Greetr = global.G$ = Greetr;
+  console.log("hi 5");
+})(window, jQuery);
+console.log("hi 5.5");
+var g = G$("Dave");
+var g2 = Greetr("John");
+console.log(g);
+console.log(g2);
